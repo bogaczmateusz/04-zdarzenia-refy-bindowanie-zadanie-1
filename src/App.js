bdaff9cf6ab5ea308bc9c166d9ca84a3f5eab2cb
Mateusz Bogacz <mateusz@martcode.pl>
2020-11-07 14:05:16 +0100

    Init.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { CalculatorFunction, CalculatorClass } from './components/Calculator';
+
+function App() {
+    return (
+        <div>
+            <CalculatorFunction />
+            <CalculatorClass />
+        </div>
+    );
+}
+
+export default App;
